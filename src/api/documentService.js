5723491f9566c1a066d73bbb4cd1e4a0ff41242c
NTPLSRPP <nathaphat.n07@gmail.com>
2024-03-06 21:58:13 +0700

    add saveDocument function

--- src/api/documentService.js
+++ src/api/documentService.js
@@ -2,8 +2,38 @@ const BASE_URL = 'http://localhost:3000/api/documents';
 
 export const getDocuments = async () => {
     const response = await fetch(BASE_URL);
-    if (response.error) {
+    if (!response.ok) {
         throw new Error('Unable to retrieve documents');
     }
     return response.json();
 }
+
+export const saveDocument = async (newDocument) => {
+    try {
+      const response = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          documents: [
+            {
+              id: newDocument.id,
+              title: newDocument.title,
+              content: newDocument.content,
+              createAt: newDocument.createAt, // Use timestamp
+            },
+          ],
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Unable to save document');
+      }
+  
+      return response.json();
+    } catch (error) {
+      console.error('Error saving document:', error.message);
+      throw error;
+    }
+  };
