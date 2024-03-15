7c6d5a061d640ebce55b27c0f17c5accce94d3c5
DekchaiHeart <110713141+DekchaiHeart@users.noreply.github.com>
2024-03-15 12:57:47 +0700

    Add Delete&SaveModal

--- src/components/Modal/DeleteModal.vue
+++ src/components/Modal/DeleteModal.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="justify-center flex flex-col items-center">
-    <h3 class="font-bold text-2xl text-red-400">Delete!</h3>
-    <p class="py-4">Are you sure to delete "{{ currentDocument.title }}" document?</p>
-    <div class="flex justify-center">
-      <button @click="deleteDoc()" class="btn btn-ghost hover:text-green-400 mr-5">Confirm</button>
-      <button @click="$emit('closeModal', false)" class="btn btn-ghost hover:text-red-400">
-        Cancel
-      </button>
+  <button class="btn btn-ghost hover:text-red-400" onclick="my_modal_3.showModal()">
+    <v-icon name="ri-delete-bin-5-fill" hover animation="ring" />
+  </button>
+  <dialog id="my_modal_3" class="modal">
+    <div class="modal-box justify-center flex flex-col items-center">
+      <h3 class="font-bold text-2xl text-red-400">Delete!</h3>
+      <p class="py-4">Are you sure to delete this document?</p>
+      <div class="flex justify-center">
+        <form method="dialog">
+          <button @click="deleteDoc()" class="btn btn-ghost hover:text-green-400 mr-5">
+            Confirm
+          </button>
+          <button class="btn btn-ghost hover:text-red-400">Cancel</button>
+        </form>
+      </div>
     </div>
-  </div>
+  </dialog>
 </template>
 <script setup>
-defineEmits(['closeModal'])
-const { deleteDoc, currentDocument } = defineProps(['deleteDoc', 'currentDocument'])
+const { deleteDoc } = defineProps(['deleteDoc'])
 </script>
