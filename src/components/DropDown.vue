8508af2911b8d19523f0e3bd6096bf6325a7b977
Sitthata <aprils96950@gmail.com>
2024-03-16 16:54:04 +0700

    Update DropDown component styling

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -3,7 +3,7 @@
     <div tabindex="0" role="" class="btn m-1">
       <slot name="dropdown-trigger"></slot>
     </div>
-    <div tabindex="0" class="dropdown-content flex flex-col gap-1 z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    <div tabindex="0" class="dropdown-content flex overflow-y-scroll max-h-64 flex-col gap-1 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
       <slot name="dropdown-content"></slot>
     </div>
   </div>
